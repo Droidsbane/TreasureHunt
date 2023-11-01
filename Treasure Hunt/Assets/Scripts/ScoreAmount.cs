4438d4dcf4612c25826767ff753d36aa6ae2c0a8
Droidsbane <116039533+Droidsbane@users.noreply.github.com>
2023-11-01 18:52:01 -0400

    HUD ready

---
+++ Treasure Hunt/Assets/Scripts/ScoreAmount.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using TMPro;
+
+public class ScoreAmount : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI mScoreAmount = null;
+
+    private int mScore;
+    private void Start()
+    {
+        mScoreAmount.text = "0";
+    }
+    private void OnEnable()
+    {
+        Actions.IncreaseScore += AddScore;
+    }
+    private void OnDisable()
+    {
+        Actions.IncreaseScore -= AddScore;
+    }
+    public void AddScore(int score)
+    {
+        mScore += score;
+        mScoreAmount.text = mScore.ToString();
+    }
+}
